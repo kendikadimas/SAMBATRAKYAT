a363dbb293c16a6773f33e263630bc5df1e78f73
kendikadimas <dimas.fazrulfalah@mhs.unsoed.ac.id>
2025-01-10 22:35:04 +0700

    fix profile

--- profile.php
+++ profile.php
@@ -169,6 +169,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                             <svg class="w-5 h-5 transform" :class="open ? 'rotate-180' : ''" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
                             </svg>
+                            
                         </button>
 
                         <!-- Dropdown Menu -->
